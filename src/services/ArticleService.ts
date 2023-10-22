28075ebb1d1ac38fbe6c4e4aa2d1095c75ff3189
darkystacks <darkymadeit@gmail.com>
2023-10-22 13:47:57 +0300

    some more adaptivity

--- src/services/ArticleService.ts
+++ src/services/ArticleService.ts
@@ -4,7 +4,7 @@ import { IArticle } from '../models/IArticle'
 export const articleApi = createApi({
 	reducerPath: 'articleApi',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://212.193.62.200:8080/api',
+		baseUrl: 'https://publishing-service-pn20.onrender.com/api',
 	}),
 	tagTypes: ['Article'],
 	endpoints: builder => ({
